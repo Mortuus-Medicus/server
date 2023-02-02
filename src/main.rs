6d8881d61e00c98b32619362926efdff3569b333
Mortuus-Medicus <mortuusmedicus@gmail.com>
2023-02-02 22:49:58 +0100

    Example from the documentation of Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Connect to a peer
+    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+
+    // Write some data.
+    stream.write_all(b"hello world!").await?;
+
+    Ok(())
 }
