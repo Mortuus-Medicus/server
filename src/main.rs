85654f25e561a5662be413e8844f12bab7861285
Mortuus-Medicus <mortuusmedicus@gmail.com>
2023-02-03 13:56:20 +0100

    It can listen for a client, I think?

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
-use std::error::Error;
+use tokio::net::TcpListener;
+
+use std::io;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // Connect to a peer
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    // Write some data.
-    stream.write_all(b"hello world!").await?;
+    match listener.accept().await {
+        Ok((_socket, addr)) => println!("new client: {:?}", addr),
+        Err(e) => println!("couldn't get client: {:?}", e),
+    }
+    
 
     Ok(())
 }
+
